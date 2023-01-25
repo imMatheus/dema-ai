4fd0d2caf5e69f88c37c0fb6bab6a22ba34fe7af
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-25 19:08:31 +0100

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { useQuery } from '@/hooks'
+import Navbar from '@/components/Navbar'
 
 function App() {
-    const data = useQuery('/beers')
-    console.log(data)
+	const data = useQuery('/beers')
+	console.log(data)
 
-    return <div className='bg-red-500'>hej</div>
+	return (
+		<div className="px-4">
+			<div className="mx-auto max-w-7xl">
+				<Navbar />
+				<h2>hek</h2>
+			</div>
+		</div>
+	)
 }
 
 export default App
